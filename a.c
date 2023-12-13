45f49d4b902af852e92a2fac89d82c6dca61fc7d
Jam <150675626+jamardines@users.noreply.github.com>
2023-12-13 23:13:30 +0800

    a.c

---
+++ a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int i;
+    
+    // (initialization; condition; update;)
+    for (i = 1; i <= 10; i = i + 2) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+
+}
