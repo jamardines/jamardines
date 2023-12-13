cbba9c8df931e9d06530dc9053a122b3f582c785
Jam <150675626+jamardines@users.noreply.github.com>
2023-12-13 23:14:05 +0800

    Create b.c

---
+++ b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int i;
+    
+    // (initialization; condition; update;)
+    for (i = 9; i >= 1; i = i - 2) {
+        printf("%d\n", i);
+    }   
+
+    return 0;
+
+}
