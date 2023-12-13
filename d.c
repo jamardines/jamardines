22e822087afeaaaa3a1bf61b30fbeeadcc415065
Jam <150675626+jamardines@users.noreply.github.com>
2023-12-13 23:17:00 +0800

    Update d.c

--- d.c
+++ d.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    // (initialization; condition; update;)
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
