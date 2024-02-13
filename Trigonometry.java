504ce66ccfe139a7b0e9221740cf7ee0bc0dd2fa
Jam <150675626+jamardines@users.noreply.github.com>
2024-02-13 09:44:09 +0800

    Trigonometry.java

---
+++ Trigonometry.java
@@ -0,0 +1,11 @@
+public class Trigonometry 
+{
+    public static void main(String[]args)
+    {
+        double sine =  Math.sin(0.5236);
+        double cosine = Math.cos(0.5236);
+        double sum = ( sine * sine) + ( cosine * cosine);
+
+        System.out.println("sine: " + sine + "\ncosine: " + cosine + "\nsum of square: " + sum);
+    }
+}
