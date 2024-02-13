cf9ebb3491c871ecf8848bdb44e06722db42060b
Jam <150675626+jamardines@users.noreply.github.com>
2024-02-13 09:44:51 +0800

    AverageRainFall.java

---
+++ AverageRainFall.java
@@ -0,0 +1,15 @@
+public class AverageRainFall 
+
+{
+    public static void main(String[]args)   
+    {
+        byte April, May, June;
+        April = 12; May = 14; June = 8;
+        float average = (April + May + June) / 3.0f;
+
+
+        System.out.println("Rainfall for April: " + April + ("\nRainfall for May: " + May) + ("\nRainfall for June: " + June) + ("\nAverage Rainfall: " + average ));
+       
+    }
+    
+}
