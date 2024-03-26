cb4355edad3bf1049125e59c463ba638a64cf0bb
Jam <150675626+jamardines@users.noreply.github.com>
2024-03-26 09:39:09 +0800

    Create ValueofaQuadradic.java

---
+++ ValueofaQuadradic.java
@@ -0,0 +1,11 @@
+public class ValueofaQuadradic 
+{
+    public static void main (String[]args) 
+{
+    double x = 4.0;
+    double result; 
+
+    result = (3 * (x * x) - 8 * x + 4);
+    System.out.println("The result is: " + result);
+    }
+}
